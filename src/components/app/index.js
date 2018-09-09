4fe6744565e212d9ab27dd35c3f07ac5be3b6b21
Alex Smith <alexsmith@MacBook-Pro-7.connect>
2018-09-09 12:44:20 +0100

    Adds routes to the app

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,3 +1,10 @@
 import React from 'react'
+import _ from 'lodash'
+import { BrowserRouter, Route } from 'react-router-dom'
+import { routes } from '~/routes'
 
-export default () => <h1>hello</h1>
+export default () => (
+  <BrowserRouter>
+    <div>{_.map(routes, (route, index) => <Route key={`route-${index}`} path={route.path} component={route.component} exact={route.exact} />)}</div>
+  </BrowserRouter>
+)
