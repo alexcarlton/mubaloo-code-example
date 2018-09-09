947b18b256608f9002e2500c596e8a1154212499
Alex Smith <alexsmith@MacBook-Pro-7.connect>
2018-09-09 15:36:44 +0100

    Adds sass lint

--- src/components/core/div/index.js
+++ src/components/core/div/index.js
@@ -1,4 +1,12 @@
 import React from 'react'
 import style from './style.scss'
 
-export default props => <div className={style.hello}>{props.children}</div>
+export default props => {
+  const { children } = props
+
+  return (
+    <div className={style.hello} {...props}>
+      {children}
+    </div>
+  )
+}
