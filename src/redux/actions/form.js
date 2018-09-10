9f1f4efdbf4952408e5eb8f6fede0bc7782d8367
Alex Smith <alexsmith@MacBook-Pro-7.connect>
2018-09-10 06:51:58 +0100

    Adds current date and time input

--- src/redux/actions/form.js
+++ src/redux/actions/form.js
@@ -5,3 +5,5 @@ export const updateName = name => ({ type: 'form/UPDATE_NAME', payload: name })
 export const updateDateOfBirth = dateOfBirth => ({ type: 'form/UPDATE_DATE_OF_BIRTH', payload: dateOfBirth })
 
 export const updateLocation = location => ({ type: 'form/UPDATE_LOCATION', payload: location })
+
+export const updateCurrentDateTime = currentDateTime => ({ type: 'form/UPDATE_CURRENT_DATE_TIME', payload: currentDateTime })
